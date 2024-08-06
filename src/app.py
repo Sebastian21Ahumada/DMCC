3e18896b76679f94611fe25f8cd447fe5ad9317d
Sebastian Ahumada <seba21ahumada@gmail.com>
2024-08-05 22:01:29 -0400

    First draf web app
    Created first draft of web app

--- src/app.py
+++ src/app.py
@@ -1,15 +1,117 @@
 # src/app.py
 import os
 import streamlit as st
-from utils.functions import read_sqlite_to_dataframe
+from utils.functions import *
+
 
 def main():
-    sqlite_db_path = os.path.join(os.path.dirname(__file__), '..','..','..', 'data', 'data.db')
-    # Read data 
-    query = "SELECT * FROM data limit 10"
-    df = read_sqlite_to_dataframe(sqlite_db_path, query)
-    
-    st.write(df.head())
+    st.title("Digital Marketing Customer Clasification 📊")
+
+
+    ##### Information #####
+
+    with st.expander("Information 📖"):
+        st.header("About the App.", divider= "gray")
+        st.subheader("Scope", divider= "red")
+        st.write(information_text("Scope"))
+        st.subheader("How to use the app", divider= "blue")
+        st.write(information_text("How to use the app"))
+
+
+    ##### Dataset Selection #####
+
+    dataset_selector = st.radio(
+    "Select your dataset",
+    ["Use a sample dataset", "Select your own dataset"],
+    horizontal= True
+    )
+
+    if dataset_selector == "Use a sample dataset":
+        file_path = os.path.join(os.path.dirname(__file__), 'data', 'test_dataset.csv')
+        df = read_dataset(file_path)
+    else:
+        uploaded_dataset = st.file_uploader("Choose a CSV file")
+        if uploaded_dataset is not None:
+            df = read_dataset(uploaded_dataset)
+
+
+    ##### Model #####
+    if dataset_selector == "Use a sample dataset" or uploaded_dataset is not None:
+        df_result = model(df)
+
+
+    ##### Sidebar Building #####
+
+    with st.sidebar:
+        st.header("Menu")
+        st.page_link("app.py", label="Home", icon="🏠")
+
+        if dataset_selector == "Use a sample dataset" or uploaded_dataset is not None:
+            filter_customer_selector = st.radio(
+                "Filter Customers",
+                ["Uplift Score", "From chart"],
+                horizontal= True
+            )
+            if filter_customer_selector == "Uplift Score":
+                uplift_score = st.number_input(
+                    "Insert a number", value=0.1, placeholder="Type Min Uplift Score"
+                )
+                x_score = 0.4
+                y_score = 0
+            else:
+                ymin = round(float(df_result['y'].min()),2)
+                ymax = round(float(df_result['y'].max()),2)
+                xmin = round(float(df_result['x'].min()),2)
+                xmax = round(float(df_result['x'].max()),2)
+                yavg = round(float(df_result['y'].mean()),2)
+                xavg = round(float(df_result['x'].mean()),2)
+                x_score = st.slider("Select minimum X score", xmin, xmax, xavg)
+                y_score = st.slider("Select minimum Y score", ymin, ymax, yavg)
+
+            
+            st.write("Profit Analysis")
+            benefit_score = st.number_input(
+                "Benefit of each conversion", value=10, placeholder="US Dollar ($)"
+            )
+            cost_score = st.number_input(
+                "Cost of each treatment", value=1, placeholder="US Dollar ($)"
+            )
+
+
+    ##### Display Dataset ##### 
+
+    if dataset_selector == "Use a sample dataset" or uploaded_dataset is not None: 
+        with st.expander("Show Dataset"):
+            st.write(df.head())
+
+
+    ##### Filter Customers #####
+
+        df_result['Profit'] = (benefit_score * df_result['p_tr']) - (cost_score * (df_result['p_tn'] + df_result['p_cr'])) - ((benefit_score + cost_score) * df_result['p_cn'])
+
+        if filter_customer_selector == "Uplift Score":
+            df_filtered = df_result[(df_result['uplift_score'] >= uplift_score)]
+        else:
+            df_filtered = df_result[(df_result['x'] > x_score) & (df_result['y'] > y_score)]
+        
+        
+        #df_filtered['Profit'] = (benefit_score * df_filtered['p_tr']) - (cost_score * (df_filtered['p_tn'] + df_filtered['p_cr'])) - ((benefit_score + cost_score) * df_filtered['p_cn'])
+
+    ##### Plot Results #####
+
+        tab1, tab2, tab3 = st.tabs(["Customer Segmentation", "Uplifting Model", "Download Data"])
+
+        with tab1:
+            display_profit_analysis(df_filtered, df_result)
+            st.plotly_chart(draw_scatter_plot(df_result,x_score,y_score))
+        with tab2:
+            display_profit_analysis(df_filtered, df_result)
+            st.plotly_chart(plot_histogram(df_filtered))
+        with tab3:
+            display_profit_analysis(df_filtered, df_result)
+            st.write(df_filtered)
+            download_dataset(df)
+            
 
 if __name__ == "__main__":
     main()
--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,6 +1,11 @@
 # src/utils/functions.py
 import pandas as pd
 import sqlite3
+import pickle
+import os
+from sklearn.decomposition import PCA
+import plotly.express as px
+import streamlit as st
 
 # Read sql and convert to dataframe
 def read_sqlite_to_dataframe(sqlite_db_path, query):
@@ -9,3 +14,175 @@ def read_sqlite_to_dataframe(sqlite_db_path, query):
     conn.close()
     
     return df
+
+# Read Sample dataset
+def read_dataset(file_path):
+    df = pd.read_csv(file_path)
+    return df
+
+## Load and use classification model
+def model(df):
+    model_path = os.path.join(os.path.dirname(__file__),  'xgboost_model.pkl')
+    with open(model_path, 'rb') as f:
+        loaded_model = pickle.load(f)
+
+
+    uplift_proba = loaded_model.predict_proba(df)
+    predictions = loaded_model.predict(df)
+
+    result = df.copy()
+
+    result['p_cn'] = uplift_proba[:, 0]
+    result['p_cr'] = uplift_proba[:, 1]
+    result['p_tn'] = uplift_proba[:, 2]
+    result['p_tr'] = uplift_proba[:, 3]
+
+    result['uplift_score'] = result.eval('\
+        p_cn/(p_cn + p_cr) \
+        + p_tr/(p_tn + p_tr) \
+        - p_tn/(p_tn + p_tr) \
+        - p_cr/(p_cn + p_cr)')
+
+    result['predictions'] = predictions
+
+
+    columns_of_interest = ['p_cn', 'p_tr', 'p_cr', 'p_tn']
+    data_to_reduce = result[columns_of_interest]
+    pca = PCA(n_components=2)
+    reduced_data = pca.fit_transform(data_to_reduce)
+    reduced_df = pd.DataFrame(reduced_data, columns=['x', 'y'])
+    result[['x', 'y']] = reduced_df
+
+    mapping = {
+    0: "Do not disturb",
+    1: "Sure thing",
+    2: "Lost Causes",
+    3: "Persuadables"
+    }
+
+    result['type'] = result['predictions'].map(mapping)
+
+    return result
+
+
+### function to draw the scatter plot with the model results
+def draw_scatter_plot(df, x, y):
+
+    color_map = {
+        "Do not disturb": "red",
+        "Sure thing": "blue",
+        "Lost Causes": "yellow",
+        "Persuadables": "green"
+    }
+
+    fig = px.scatter(df, x='x', y='y', color = 'type',title='Visualization of customers types',
+                    color_discrete_map=color_map)
+
+    x_min = df['x'].min()
+    x_max = df['x'].max()
+    y_min = df['y'].min()
+    y_max = df['y'].max()
+
+    fig.add_shape(
+        type="line",
+        x0=x,
+        x1=x,
+        y0=y_min,  # Extiende la línea a lo largo de todo el eje y
+        y1=y_max,
+        line=dict(color="Black", width=2, dash="dash")
+    )
+
+    fig.add_shape(
+        type="line",
+        x0=x_min,
+        x1=x_max,
+        y0=y,  # Extiende la línea a lo largo de todo el eje y
+        y1=y,
+        line=dict(color="Black", width=2, dash="dash")
+    )
+
+    return fig
+
+def plot_histogram(df):
+    fig_histogram = px.histogram(
+        df, 
+        x='uplift_score', 
+        title='Histograma de uplift_score',
+        labels={'uplift_score': 'Uplift Score'}  
+    )
+    return fig_histogram
+
+
+
+### Profit Analysis
+def display_profit_analysis(df_filtered, df_result):
+    st.subheader("Profit Analysis")
+    st.write("How much am I earning based on the filtered customers compared to the total customers in the dataset")
+
+    ## Metrics
+    profit_score = round(float(df_filtered['Profit'].sum()), 2)
+    profit_score0 = round(float(df_result['Profit'].sum()), 2)
+    profit_score_delta = profit_score - profit_score0
+
+    total_customer = int(df_filtered['Profit'].count())
+    total_customer0 = int(df_result['Profit'].count())
+    total_customer_delta = total_customer - total_customer0
+
+    total_customer_do_not_disturb = int(df_filtered[df_filtered['type'] == "Do not disturb"]['Profit'].count())
+    total_customer_do_not_disturb0 = int(df_result[df_result['type'] == "Do not disturb"]['Profit'].count())
+    total_customer_do_not_disturb_delta = total_customer_do_not_disturb - total_customer_do_not_disturb0
+
+    total_customer_lost_causes = int(df_filtered[df_filtered['type'] == "Lost Causes"]['Profit'].count())
+    total_customer_lost_causes0 = int(df_result[df_result['type'] == "Lost Causes"]['Profit'].count())
+    total_customer_lost_causes_delta = total_customer_lost_causes - total_customer_lost_causes0
+
+    total_customer_persuadables = int(df_filtered[df_filtered['type'] == "Persuadables"]['Profit'].count())
+    total_customer_persuadables0 = int(df_result[df_result['type'] == "Persuadables"]['Profit'].count())
+    total_customer_persuadables_delta = total_customer_persuadables - total_customer_persuadables0
+
+    total_customer_sure_thing = int(df_filtered[df_filtered['type'] == "Sure thing"]['Profit'].count())
+    total_customer_sure_thing0 = int(df_result[df_result['type'] == "Sure thing"]['Profit'].count())
+    total_customer_sure_thing_delta = total_customer_sure_thing - total_customer_sure_thing0
+
+    col1, col2, col3, col4, col5, col6 = st.columns(6)
+
+    col1.metric(label="Profit", value="$" + str(profit_score), delta="$" + str(profit_score_delta))
+    col2.metric(label="Total Customers", value=total_customer, delta=total_customer_delta, delta_color="off")
+    col3.metric(label="Do not disturb", value=total_customer_do_not_disturb, delta=total_customer_do_not_disturb_delta, delta_color="inverse")
+    col4.metric(label="Lost Causes", value=total_customer_lost_causes, delta=total_customer_lost_causes_delta, delta_color="inverse")
+    col5.metric(label="Persuadable", value=total_customer_persuadables, delta=total_customer_persuadables_delta)
+    col6.metric(label="Sure thing", value=total_customer_sure_thing, delta=total_customer_sure_thing_delta, delta_color="inverse")
+
+
+def convert_df(df):
+    return df.to_csv().encode("utf-8")
+
+def download_dataset(df):
+    csv = convert_df(df)
+    st.download_button(
+        label="Download data as CSV",
+        data=csv,
+        file_name="large_df.csv",
+        mime="text/csv",
+    )
+
+def information_text(text):
+    if text == 'Scope':
+        display_text = """The objective of the web application is to classify potential customers obtained from a 
+                 marketing campaign database. This will allow us to identify, based on certain characteristics. The app
+                 used a modeling technique called uplift, which is an important yet novel area of research in machine learning 
+                 which aims to explain and to estimate the causal impact of a treatment at the individual level.
+                 """
+    elif text == 'How to use the app':
+        display_text = """ 
+        The general use of the application is as follows:
+
+        **1.- Load a dataset:** The app has a pre-loaded test dataset that can be used, but it also offers the possibility to load a custom dataset. The pre-loaded dataset consists of 12 columns, ranging from f1 to f12, representing the features to be used for customer classification. In real data, the features may correspond to such things as customer purchase history, demographics, and other quantities a data scientist may engineer with the hypothesis that they would be useful in modeling uplift.
+        
+        **2.- Set the desired parameters:** Once the dataset is loaded, menu bar parameters appear that can be manipulated by the user. The idea is to test different combinations to filter the final customers according to the model's results. Additionally, a profit analysis is provided based on the user-defined revenue and costs.
+        
+        **3.- UI Area:** As the user changes the parameters in the sidebar menu, they can see how it affects the customers in the UI area, located in the center of the app. 
+        There are three tabs available. The **Customer Segmentation** tab display a scatter plot shows the distribution of different customers based on the model's predictions and which group they belong to. 
+        The **Uplifting Model** display the histogram with the uplift modeling results, and finally, the **Download data** tab show the dataset with the final list of filtered customers to be used in the marketing campaign, based on the parameters selected by the user. """
+
+    return display_text
